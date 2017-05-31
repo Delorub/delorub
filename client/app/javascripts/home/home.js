ae822ecc054625d17e2b3fe0ccfaf1f5887c388c
Amstin <amstinf@gmail.com>
2017-06-01 02:37:19 +0800

    notification

--- client/app/javascripts/home/home.js
+++ client/app/javascripts/home/home.js
@@ -248,3 +248,13 @@ $(document).ready(function () {
 })(window);
 
 new UISearch(document.getElementById('sb-search'));
+
+jQuery(function ($) {
+    $(document).mouseup(function (e) { // событие клика по веб-документу
+        var div = $("#open-notification"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+                && div.has(e.target).length === 0) { // и не по его дочерним элементам
+            div.hide(); // скрываем его
+        }
+    });
+});
