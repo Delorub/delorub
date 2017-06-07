c8fa3246b6fdfd4758cedf261e26f7ba4bdf3d15
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-06-07 12:59:47 +0300

    add skipping for elasticsearch tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ RSpec.configure do |config|
   # Ensure that if we are running js tests, we are using latest webpack assets
   # This will use the defaults of :js and :server_rendering meta tags
   ReactOnRails::TestHelper.configure_rspec_to_compile_assets(config)
+  config.filter_run_excluding search: true
 
   config.include Rails.application.routes.url_helpers
 
