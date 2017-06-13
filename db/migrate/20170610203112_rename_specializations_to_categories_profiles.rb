10f1c7b08be3a38ffbad84da287fb96bd5a7e6b7
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-06-13 17:30:42 +0300

    add todos for migrations

--- db/migrate/20170610203112_rename_specializations_to_categories_profiles.rb
+++ db/migrate/20170610203112_rename_specializations_to_categories_profiles.rb
@@ -1,4 +1,5 @@
 class RenameSpecializationsToCategoriesProfiles < ActiveRecord::Migration
+  # TODO delete after production release
   def change
     rename_table :specializations, :categories_profiles
   end
