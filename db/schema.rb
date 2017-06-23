b1805e39e3ecc122d3b01daabb3fdf38838f9265
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-06-23 13:10:24 +0300

    fix migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622134930) do
+ActiveRecord::Schema.define(version: 20170622183630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -257,7 +257,7 @@ ActiveRecord::Schema.define(version: 20170622134930) do
   end
 
   add_index "portfolio_items", ["profile_id"], name: "index_portfolio_items_on_profile_id", using: :btree
-  
+
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id",                                     null: false
     t.string   "work_type"
