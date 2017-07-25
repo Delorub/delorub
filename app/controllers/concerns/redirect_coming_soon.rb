72e2a5284ad7afedaf585cbf2541c956eb361c09
delphist <xdelphist@gmail.com>
2017-07-25 21:38:54 +0800

    Fix tests

--- app/controllers/concerns/redirect_coming_soon.rb
+++ app/controllers/concerns/redirect_coming_soon.rb
@@ -8,6 +8,7 @@ module RedirectComingSoon
   protected
 
     def check_and_redirect_coming_soon
+      return if self.class.parent == Admin
       return if controller_name.in? %w[main coming_soon_sessions pages]
       redirect_to new_coming_soon_session_path
     end
