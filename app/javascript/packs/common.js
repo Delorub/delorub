16e58b3de8b799b5bb5f8a3bb9fc61f7cac9482d
Amstin <amstinf@gmail.com>
2017-07-27 02:02:49 +0900

    Features/create task mod (#65)
    * fixed footer, inputs styles,changed navbar, breadcrumbs styles, block styles, added slider
    
    * checkbox styles, slider styles
    
    * hot fix
    
    * fixed checkbox, background-image, added coming soon after
    
    * popovers, create task
    
    * fixes,sticky footer, favicon, logo

--- app/javascript/packs/common.js
+++ app/javascript/packs/common.js
@@ -2,16 +2,29 @@ import $ from 'jquery'
 import 'bootstrap-slider/dist/bootstrap-slider.js'
 import 'bootstrap-slider/dist/css/bootstrap-slider.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import 'ion-rangeslider/js/ion.rangeSlider.js'
+import 'ion-rangeslider/css/ion.rangeSlider.css'
+import 'ion-rangeslider/css/ion.rangeSlider.skinHTML5.css'
 import 'app'
-
-$('#ex1').slider({
-  formatter: function (value) {
-    return 'Current value: ' + value
-  }
-})
 
 $('#ex2').slider({
   formatter: function (value) {
     return 'Current value: ' + value
   }
+})
+
+$('#budget_slider').ionRangeSlider(
+  {
+    grid: true,
+    from: 1,
+    values: [
+      '500', '1 500',
+      '5 000', '15 000',
+      '50 000', '100 000',
+      '200 000', '300 000'
+    ]
+  })
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
 })
