34d43d3804555e52e2b357444a8b56399e0209d3
denis <denis_shvets@mail.ru>
2017-08-22 18:01:40 +0300

    fix views by changes in models

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -1,4 +1,5 @@
 class ProfileDecorator < Draper::Decorator
+  include ActionView::Helpers::DateHelper
   decorates Profile
   delegate_all
 
@@ -8,10 +9,19 @@ class ProfileDecorator < Draper::Decorator
     object.categories.first.id
   end
 
-  def price
-    case object.price_type
-      when 'hourly' then I18n.t(:profile_price, price: object.price_hourly)
-      when 'project' then I18n.t(:profile_price, price: object.price_project)
-    end
+  def age
+    object.birthday.present? ? distance_of_time_in_words(object.birthday, DateTime.current) : nil
+  end
+
+  def formatted_age
+    age.present? ? age : I18n.t(:not_specified)
+  end
+
+  def formatted_city_name
+    object.city_name.present? ? object.city_name : I18n.t(:not_specified)
+  end
+
+  def formatted_price
+    object.price_project.present? ? I18n.t(:profile_price, price: object.price_project) : I18n.t(:price_unknown)
   end
 end
