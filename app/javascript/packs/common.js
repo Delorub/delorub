872589ffa99e9bd04bc892c2fa8351391e6211a4
Amstin <you@example.com>
2017-08-29 01:30:25 +0800

    TG-62 authorized hat

--- app/javascript/packs/common.js
+++ app/javascript/packs/common.js
@@ -1,2 +1,9 @@
 import 'app'
+import $ from 'jquery'
 import 'bootstrap/dist/js/bootstrap.js'
+
+$('div.dr-authorized-user').hover(function () {
+  $('div.dr-authorized-user__dropdown').stop(true, true).delay(100).fadeIn(300)
+}, function () {
+  $('div.dr-authorized-user__dropdown').stop(true, true).delay(100).fadeOut(300)
+})
