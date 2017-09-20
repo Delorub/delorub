be0f7ee818b95ceddaca7297fac6d38b9a6af1be
Amstin <you@example.com>
2017-09-20 23:44:42 +0800

    added tooltip to button

--- app/javascript/packs/common.js
+++ app/javascript/packs/common.js
@@ -23,57 +23,64 @@ $(function () {
   })
 })
 
-var ctx = document.getElementById('myChart').getContext('2d')
-// eslint-disable-next-line no-new
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-    datasets: [{
-      data: [2, 5, 7.5, 11, 17, 25, 33, 57, 85, 128],
-      backgroundColor: [
-        '#9bd4f4',
-        '#9bd4f4',
-        '#00bfd8',
-        '#e6e8e8',
-        '#e6e8e8',
-        '#e6e8e8',
-        '#e6e8e8',
-        '#e6e8e8',
-        '#e6e8e8',
-        '#e6e8e8'
-      ],
-      borderColor: [
-        '#9bd4f4',
-        '#9bd4f4',
-        '#00bfd8',
-        '#e6e8e8',
-        '#e6e8e8',
-        '#e6e8e8',
-        '#e6e8e8',
-        '#e6e8e8',
-        '#e6e8e8',
-        '#e6e8e8'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    legend: {
-      display: false
-    },
-    tooltips: {
-      cornerRadius: 3,
-      position: 'average',
-      caretPadding: 0,
-      caretSize: 10
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
 })
+
+let ctx = document.getElementById('myChart')
+if (ctx !== undefined) {
+  ctx = ctx.getContext('2d')
+  // eslint-disable-next-line no-new
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+      datasets: [{
+        data: [2, 5, 7.5, 11, 17, 25, 33, 57, 85, 128],
+        backgroundColor: [
+          '#9bd4f4',
+          '#9bd4f4',
+          '#00bfd8',
+          '#e6e8e8',
+          '#e6e8e8',
+          '#e6e8e8',
+          '#e6e8e8',
+          '#e6e8e8',
+          '#e6e8e8',
+          '#e6e8e8'
+        ],
+        borderColor: [
+          '#9bd4f4',
+          '#9bd4f4',
+          '#00bfd8',
+          '#e6e8e8',
+          '#e6e8e8',
+          '#e6e8e8',
+          '#e6e8e8',
+          '#e6e8e8',
+          '#e6e8e8',
+          '#e6e8e8'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      tooltips: {
+        cornerRadius: 3,
+        position: 'average',
+        caretPadding: 0,
+        caretSize: 10
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  })
+}
