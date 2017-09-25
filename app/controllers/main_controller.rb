6205b59742614c31a059ab859deba6f7cc6f022b
denis <denis_shvets@mail.ru>
2017-09-25 11:54:15 +0300

    robots by env

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,11 @@ class MainController < ApplicationController
 
   def index; end
 
+  def robots
+    robots = File.read(Rails.root + "config/robots/#{Rails.env}.txt")
+    render :plain => robots, :layout => false, :content_type => 'text/plain'
+  end
+
   private
 
     def random_categories
