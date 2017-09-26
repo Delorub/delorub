d766670fa598d687f4cca0fa1fe51dd032a21bc5
delphist <xdelphist@gmail.com>
2017-09-26 17:14:15 +0800

    Fix current_user bug

--- app/controllers/yandex_kassa_callback_controller.rb
+++ app/controllers/yandex_kassa_callback_controller.rb
@@ -2,9 +2,10 @@ class YandexKassaCallbackController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def processing
-    run Billing::YandexKassa::Deposit::Operation::Finish, params do |result|
+    run Billing::YandexKassa::Deposit::Operation::Finish, params, {} do |result|
       return render plain: Billing::YandexKassa::ResponseService.new(params['invoiceId'], 0).aviso_response
     end
+
     render plain: Billing::YandexKassa::ResponseService.new(params['invoiceId'], 1).aviso_response
   end
 
@@ -12,6 +13,7 @@ class YandexKassaCallbackController < ApplicationController
     run Billing::YandexKassa::Deposit::Operation::Check, params do |result|
       return render plain: Billing::YandexKassa::ResponseService.new(params['invoiceId'], 0).check_response
     end
+
     render plain: Billing::YandexKassa::ResponseService.new(params['invoiceId'], 100).check_response
   end
 end
