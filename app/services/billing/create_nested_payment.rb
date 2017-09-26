d60d1dc2f66f0abd55403b531bbc213f8e5f69df
delphist <xdelphist@gmail.com>
2017-09-26 17:22:25 +0800

    Rubocop fixes

--- app/services/billing/create_nested_payment.rb
+++ app/services/billing/create_nested_payment.rb
@@ -1,30 +1,32 @@
 class Billing::CreateNestedPayment
   extend Uber::Callable
 
-  def self.call options, model:, **_
-    return true unless model.pay_type.to_sym.in? payment_types
-    amount = -options['sum'].to_i
+  class << self
+    def call options, model:, **_
+      return true unless model.pay_type.to_sym.in? payment_types
+      amount = -options['sum'].to_i
 
-    options['nested_payment'] = Billing::YandexKassa::Deposit::Operation::Create.call(
-      {
-        amount: amount,
-        pay_type: model.pay_type.to_s,
-        accept_terms: '1'
-      },
-      'current_user' => options['current_user']
-    )
+      options['nested_payment'] = Billing::YandexKassa::Deposit::Operation::Create.call(
+        {
+          amount: amount,
+          pay_type: model.pay_type.to_s,
+          accept_terms: '1'
+        },
+        'current_user' => options['current_user']
+      )
 
-    # TODO: move to operation
-    if options['nested_payment'].success?
+      # TODO: move to operation
+      return false if options['nested_payment'].failure?
+
       options['nested_payment']['model'].billing_log.finish_after_billing_log_id = model.billing_log.id
       options['nested_payment']['model'].billing_log.save!
       true
     end
+
+    private
+
+      def payment_types
+        Billing::PaymentTypeList.for(Billing::YandexKassa::Deposit)
+      end
   end
-
-  private
-
-    def self.payment_types
-      Billing::PaymentTypeList.for(Billing::YandexKassa::Deposit)
-    end
 end
