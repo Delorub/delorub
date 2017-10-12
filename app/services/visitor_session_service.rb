625fae95f8d582c463d3563d03effbb43a18ac00
Amstin <amstinf@gmail.com>
2017-10-12 17:08:01 +0800

    fix to icons (#157)
    * fix to icons
    
    * footer remastered
    
    * fix to contacts
    
    * hotfix
    
    * hot fix
    
    * fix to mail
    
    * fix

--- app/services/visitor_session_service.rb
+++ app/services/visitor_session_service.rb
@@ -34,7 +34,6 @@ class VisitorSessionService
   def model
     @model ||= VisitorSession.find_or_initialize_by(id: cookies.permanent.encrypted[:visitor_session_id]) do |visitor_session|
       visitor_session.ip = request.ip
-
       begin
         visitor_session.city = Ipgeobase.lookup(request.ip).city
       rescue StandardError
