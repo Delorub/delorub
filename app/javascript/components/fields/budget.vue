68b2f739e30e12c99ef7b79273e1ddc228e9e4e0
Amstin <amstinf@gmail.com>
2017-10-31 00:01:55 +0800

    Bug/sprint 4 bugs hotfixes 2 (#167)
    * fix to budget
    
    * hotfix to datepicker
    
    * hotfix
    
    * fix to datepicker
    
    * some fix to select
    
    * hot fix
    
    * fix to date
    
    * fix to footer
    
    * fix to footer
    
    * fix to profile
    
    * hot fix
    
    * fix to private office
    
    * fix to 404
    
    * fixes
    
    * fix to navbar
    
    * fix to buttons
    
    * fix to dropzone
    
    * try to fix time
    
    * fix to seed.rb
    
    * 2 inputs for time
    
    * fix to 2 time inputs
    
    * styled 2 inputs of time
    
    * hotfix
    
    * fixed time inputs
    
    * hotfix
    
    * fix to styles of time
    
    * fix to budget
    
    * todo-list
    
    * fix to budget
    
    * hotfix

---
+++ app/javascript/components/fields/budget.vue
@@ -0,0 +1,28 @@
+<template lang="pug">  
+  input.form-control.string.required(
+    type="text"
+    ref="priceInput"
+    maxlength="15"
+    placeholder="300 000"
+  )
+</template>
+
+<script>
+  import Cleave from 'cleave.js'
+
+  export default {
+    data () {
+      return {
+        priceCleave: null,
+        priceCleaveConfig: {
+          numericOnly: true,
+          blocks: [3, 3, 3, 3],
+          delimeter: ' '
+        }
+      }
+    },
+    mounted () {
+      this.priceCleave = new Cleave(this.$refs.priceInput, this.priceCleaveConfig)
+    }
+  }
+</script>
